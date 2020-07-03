b16e5296b9a9bcc19e220d55411c77ed32aa4250
vatsal-thakor-aui-fsb-1 <thakorvatsal@gmail.com>
2020-07-02 20:36:11 -0400

    week4 day3 assignment

---
+++ Assignments/Week4/Day3/my-company/index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = require('path');
+app.use(express.static('public'));
+
+app.listen(8080,function(){
+    console.log("working");
+})
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/public/index.html")
+})
+app.get('/team',function(req,res){
+    res.sendFile(__dirname + "/public/team.html")
+})
+app.get('/contact-us',function(req,res){
+    res.sendFile(__dirname + "/public/contact-us.html")
+})
+
+app.post('/contact-us',(req,res)=>{
+    res.send('<h1>Thank you</h1>');
+})
